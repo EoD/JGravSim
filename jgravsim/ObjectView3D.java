5ab76e3dd56dd2c235af3a171b2fa562b95f05fb
EoD <EoD@gmx.net>
2010-08-02 17:11:01 +0200

    *Codechange: MAJOR overhaul of ObjectView3D *Fix: ObjectView3D does not automatically reset after each frame

--- jgravsim/ObjectView3D.java
+++ jgravsim/ObjectView3D.java
@@ -14,6 +14,7 @@ import javax.media.j3d.Texture;
 import javax.media.j3d.Texture2D;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
+import javax.media.j3d.TransparencyAttributes;
 
 import javax.vecmath.*;
 
@@ -33,6 +34,7 @@ public class ObjectView3D extends ObjectView {
 	private BranchGroup bg_main;
 	private Canvas3D canvas_main;
 	private SimpleUniverse simpleU;
+	private TransformGroup[] tg_masspoints;
 	
 	
 	ObjectView3D(Step current) {
@@ -46,34 +48,29 @@ public class ObjectView3D extends ObjectView {
 
 		setLayout(new BorderLayout());
 		setBackground(Color.white);
-		GraphicsConfiguration config = SimpleUniverse
-				.getPreferredConfiguration();
-
+		GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
 		canvas_main = new Canvas3D(config);
 		add("Center", canvas_main);
 
 		// SimpleUniverse is a Convenience Utility class
 		simpleU = new SimpleUniverse(canvas_main);
 
-		bg_main = createSceneGraph(simpleU);
-
-		// This will move the ViewPlatform back a bit so the
-		// objects in the scene can be viewed.
-		simpleU.getViewingPlatform().setNominalViewingTransform();
-		simpleU.addBranchGraph(bg_main);
 		// str_clickme = "";
 		//repaint();
 	}
 	
 	public BranchGroup createSceneGraph(SimpleUniverse su) {
+		debugout("createSceneGraph() - "+su.toString());
+		
 		// Create the root of the branch graph
 		BranchGroup objRoot = new BranchGroup();
 		objRoot.setCapability( BranchGroup.ALLOW_DETACH);
 		TransformGroup vpTrans = null;
 		BoundingSphere mouseBounds = null;
 
+        Transform3D T3D = new Transform3D();
 		vpTrans = su.getViewingPlatform().getViewPlatformTransform();
-		
+		vpTrans.getTransform(T3D);
 
 		///TEXTURE////
 		String texturefile_earth = "earth.jpg";
@@ -95,8 +92,6 @@ public class ObjectView3D extends ObjectView {
 		texture_bh.setImage(0, image_bh);
 		
 		
-		
-		
 		///TRANSFORMATION I
 		TransformGroup objRotate = new TransformGroup();
 		objRotate.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
@@ -105,47 +100,50 @@ public class ObjectView3D extends ObjectView {
 		///TRANSOFMRATION II - TRANSLATION
 		if (stCurrent != null && stCurrent.getMasspoints() != null && stCurrent.getMasspoints().length > 0) {
 			Masspoint_Sim[] masspoints = stCurrent.getMasspoints();
-			for (int i = 0; i < masspoints.length; i++) {
+			int num_masspoints = masspoints.length;
+			tg_masspoints = new TransformGroup[num_masspoints];
+			
+			for (int i = 0; i < num_masspoints; i++) {
 				Masspoint_Sim masspoint = masspoints[i];
+				tg_masspoints[i] = new TransformGroup();
+				//tg_masspoints[i].setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+				tg_masspoints[i].setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 				
 				if(masspoint == null)
 					continue;
 
-				Appearance appear = new Appearance();
 				double radius = masspoint.getRadius();
 				if(radius/CONVERT3D < 0.05f)
 					radius = 0.05f*CONVERT3D;
 				
-				if(masspoint.isBlackHole()) {
-					//Controller.debugout("Object3DView - Blackhole");
+				
+				Appearance appear = new Appearance();
+				if(masspoint.isBlackHole())
 					appear.setTexture(texture_bh);
-				}
-				else {
-					//Controller.debugout("Object3DView - Earth");
-					appear.setTexture(texture_earth);	
-				}
+				else
+					appear.setTexture(texture_earth);
+				
+				TransparencyAttributes transparency = new TransparencyAttributes(TransparencyAttributes.FASTEST,0.0f);
+				transparency.setCapability(TransparencyAttributes.ALLOW_VALUE_WRITE);
+				appear.setTransparencyAttributes(transparency);
 				
 				//TransformGroup objTranslationSub = new TransformGroup();
 				Transform3D TranslationSub = new Transform3D();
 
-				Vector3d vector = new Vector3d(
-						MVMath.ConvertToD(masspoint.getPosX())/CONVERT3D/iZoomLevel, 
-						MVMath.ConvertToD(masspoint.getPosY())/CONVERT3D/iZoomLevel,  
-						MVMath.ConvertToD(masspoint.getPosZ())/CONVERT3D/iZoomLevel);
-				
+				Vector3d vector = postovector3d(masspoint);
 				//Controller.debugout("createSceneGraph() - masspoint.getPosX()/CalcCode.LACCURACY/CONVERT3D/iZoomLevel="+masspoint.getPosX()/CalcCode.LACCURACY/CONVERT3D/iZoomLevel);
 				
 				TranslationSub.setTranslation(vector);
-				TransformGroup yoyoTGT1 = new TransformGroup(TranslationSub);
-				objRotate.addChild(yoyoTGT1);
+				tg_masspoints[i].setTransform(TranslationSub);
+				objRotate.addChild(tg_masspoints[i]);
 				
 				Sphere sphere = new Sphere((float)(radius/CONVERT3D/iZoomLevel),Primitive.GENERATE_TEXTURE_COORDS, appear);
-				sphere.setCapability( BranchGroup.ALLOW_DETACH);
+				//sphere.setCapability( BranchGroup.ALLOW_DETACH);
 				
 				//objTranslationSub.setTransform(TranslationSub);
 				//objTranslationSub.addChild(sphere);		    
 				sphere.setAppearance(appear);
-			    yoyoTGT1.addChild(sphere);
+				tg_masspoints[i].addChild(sphere);
 			}
 		}
 	    ///TRANSFORMATION III - ROTATION
@@ -178,9 +176,12 @@ public class ObjectView3D extends ObjectView {
 
 
         Vector3f translate = new Vector3f();
-        Transform3D T3D = new Transform3D();
         translate.set( 0.0f, 0.03f, 0.0f);
-        T3D.setTranslation(translate);
+        Transform3D translation = new Transform3D();
+        translation.setTranslation(translate);
+        
+        T3D.mul(translation);
+      
         vpTrans.setTransform(T3D);
         KeyNavigatorBehavior keyNavBeh = new KeyNavigatorBehavior(vpTrans);
         keyNavBeh.setSchedulingBounds(new BoundingSphere(new Point3d(),1000.0));
@@ -197,20 +198,64 @@ public class ObjectView3D extends ObjectView {
 	}
 	
 	public void updateSceneGraph() {
-		BranchGroup bg_new = createSceneGraph(simpleU);
-		//bg_main.compile();
-		simpleU.getLocale().replaceBranchGraph(bg_main, bg_new);
-		bg_main = bg_new;
-		simpleU.getViewingPlatform().setNominalViewingTransform();
-
+		if(tg_masspoints == null) {
+			bg_main = createSceneGraph(simpleU);
+			simpleU.getViewingPlatform().setNominalViewingTransform();
+			simpleU.addBranchGraph(bg_main);
+		}
+		else if (stCurrent != null && stCurrent.getMasspoints() != null && stCurrent.getMasspoints().length > 0) {
+			Masspoint_Sim[] masspoints = stCurrent.getMasspoints();
+			for(int i=0; i<masspoints.length && i<tg_masspoints.length; i++) {
+				Transform3D translation = new Transform3D();
+				translation.setTranslation( postovector3d(masspoints[i]) );
+				tg_masspoints[i].setTransform(translation);
+				makempvisibile(tg_masspoints[i], true);
+			}
+			//If we have more transform groups than masspoints, hide all of them
+			if(tg_masspoints.length > masspoints.length) {
+				for(int i=masspoints.length; i<tg_masspoints.length; i++) {
+					makempvisibile(tg_masspoints[i], false);
+				}
+			}
+			//FIXME The following case should be fixed
+			else if(tg_masspoints.length < masspoints.length) {
+				debugout("updateSceneGraph() - FIXME: tg_masspoints.length < masspoints.length");
+			}
+		}
 	}
 
 	@Override
 	void displayStep(Step next) {
 		stCurrent = next;
-		updateSceneGraph();
+		if(stCurrent == null) {
+			tg_masspoints = null;
+			simpleU.getLocale().removeBranchGraph(bg_main);
+		}
+		else {
+			stCurrent.sort();
+			updateSceneGraph();
+		}
 	}
 
+	private Vector3d postovector3d(Masspoint_Sim masspoint) {
+		Vector3d vector = new Vector3d(
+				MVMath.ConvertToD(masspoint.getPosX())/CONVERT3D/iZoomLevel, 
+				MVMath.ConvertToD(masspoint.getPosY())/CONVERT3D/iZoomLevel,  
+				MVMath.ConvertToD(masspoint.getPosZ())/CONVERT3D/iZoomLevel);
+		
+		return vector;
+	}
+	
+	private void makempvisibile(TransformGroup tg_masspoint, boolean visible) {
+		//There is only one child at the moment
+		Sphere sphere = (Sphere) tg_masspoint.getChild(0);
+		Appearance appear = sphere.getAppearance();
+		TransparencyAttributes transparency = appear.getTransparencyAttributes();
+		if(visible)
+			transparency.setTransparency(0.0f);
+		else
+			transparency.setTransparency(1.0f);
+	}
 	/*
 	 * void drawClickmeOverlay(Graphics g, int centerX, int centerY) { // int
 	 * size = 48; Font arialFont = new Font("Arial", Font.ITALIC, 22);
