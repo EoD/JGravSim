6b5caeb7f8ebbfa18f95987df5e9f700b2402236
EoD <EoD@gmx.net>
2010-08-05 17:32:41 +0200

    *Feature: made axis gray, half-invisible and much more beautiful

--- jgravsim/Axis.java
+++ jgravsim/Axis.java
@@ -1,4 +1,5 @@
 package jgravsim;
+
 /*
  *      Axis.java 1.0 98/11/25
  *
@@ -41,80 +42,78 @@ package jgravsim;
  *   2. Using LineArray to draw 3D lines.
  */
 
-
 import javax.media.j3d.*;
 import javax.vecmath.*;
 
-
-    public class Axis extends Shape3D{
+public class Axis extends Shape3D {
 
 	////////////////////////////////////////////
 	//
 	// create axis visual object
 	//
 	public Axis() {
-	
-	    this.setGeometry(createGeometry());
+		this.setGeometry(createGeometry());
 
 	}
 
-	private Geometry createGeometry(){
-	    // create line for X axis
-            IndexedLineArray axisLines = new IndexedLineArray(18, GeometryArray.COORDINATES, 30);
+	private Geometry createGeometry() {
+		// create line for X axis
+		IndexedLineArray axisLines = new IndexedLineArray(18, GeometryArray.COORDINATES | GeometryArray.COLOR_4, 30);
 
-	    axisLines.setCoordinate( 0, new Point3f(-1.0f, 0.0f, 0.0f));
-	    axisLines.setCoordinate( 1, new Point3f( 1.0f, 0.0f, 0.0f));
-	    axisLines.setCoordinate( 2, new Point3f( 0.9f, 0.1f, 0.1f));
-	    axisLines.setCoordinate( 3, new Point3f( 0.9f,-0.1f, 0.1f));
-	    axisLines.setCoordinate( 4, new Point3f( 0.9f, 0.1f,-0.1f));
-	    axisLines.setCoordinate( 5, new Point3f( 0.9f,-0.1f,-0.1f));
-	    axisLines.setCoordinate( 6, new Point3f( 0.0f,-1.0f, 0.0f));
-	    axisLines.setCoordinate( 7, new Point3f( 0.0f, 1.0f, 0.0f));
-	    axisLines.setCoordinate( 8, new Point3f( 0.1f, 0.9f, 0.1f));
-	    axisLines.setCoordinate( 9, new Point3f(-0.1f, 0.9f, 0.1f));
-	    axisLines.setCoordinate(10, new Point3f( 0.1f, 0.9f,-0.1f));
-	    axisLines.setCoordinate(11, new Point3f(-0.1f, 0.9f,-0.1f));
-	    axisLines.setCoordinate(12, new Point3f( 0.0f, 0.0f,-1.0f));
-	    axisLines.setCoordinate(13, new Point3f( 0.0f, 0.0f, 1.0f));
-	    axisLines.setCoordinate(14, new Point3f( 0.1f, 0.1f, 0.9f));
-	    axisLines.setCoordinate(15, new Point3f(-0.1f, 0.1f, 0.9f));
-	    axisLines.setCoordinate(16, new Point3f( 0.1f,-0.1f, 0.9f));
-	    axisLines.setCoordinate(17, new Point3f(-0.1f,-0.1f, 0.9f));
+		axisLines.setCoordinate(0, new Point3f(-1.0f, 0.0f, 0.0f));
+		axisLines.setCoordinate(1, new Point3f(1.0f, 0.0f, 0.0f));
+		axisLines.setCoordinate(2, new Point3f(0.94f, 0.02f, 0.02f));
+		axisLines.setCoordinate(3, new Point3f(0.94f, -0.02f, 0.02f));
+		axisLines.setCoordinate(4, new Point3f(0.94f, 0.02f, -0.02f));
+		axisLines.setCoordinate(5, new Point3f(0.94f, -0.02f, -0.02f));
+		axisLines.setCoordinate(6, new Point3f(0.0f, -1.0f, 0.0f));
+		axisLines.setCoordinate(7, new Point3f(0.0f, 1.0f, 0.0f));
+		axisLines.setCoordinate(8, new Point3f(0.02f, 0.94f, 0.02f));
+		axisLines.setCoordinate(9, new Point3f(-0.02f, 0.94f, 0.02f));
+		axisLines.setCoordinate(10, new Point3f(0.02f, 0.94f, -0.02f));
+		axisLines.setCoordinate(11, new Point3f(-0.02f, 0.94f, -0.02f));
+		axisLines.setCoordinate(12, new Point3f(0.0f, 0.0f, -1.0f));
+		axisLines.setCoordinate(13, new Point3f(0.0f, 0.0f, 1.0f));
+		axisLines.setCoordinate(14, new Point3f(0.02f, 0.02f, 0.94f));
+		axisLines.setCoordinate(15, new Point3f(-0.02f, 0.02f, 0.94f));
+		axisLines.setCoordinate(16, new Point3f(0.02f, -0.02f, 0.94f));
+		axisLines.setCoordinate(17, new Point3f(-0.02f, -0.02f, 0.94f));
 
-            axisLines.setCoordinateIndex( 0, 0);
-            axisLines.setCoordinateIndex( 1, 1);
-            axisLines.setCoordinateIndex( 2, 2);
-            axisLines.setCoordinateIndex( 3, 1);
-            axisLines.setCoordinateIndex( 4, 3);
-            axisLines.setCoordinateIndex( 5, 1);
-            axisLines.setCoordinateIndex( 6, 4);
-            axisLines.setCoordinateIndex( 7, 1);
-            axisLines.setCoordinateIndex( 8, 5);
-            axisLines.setCoordinateIndex( 9, 1);
-            axisLines.setCoordinateIndex(10, 6);
-            axisLines.setCoordinateIndex(11, 7);
-            axisLines.setCoordinateIndex(12, 8);
-            axisLines.setCoordinateIndex(13, 7);
-            axisLines.setCoordinateIndex(14, 9);
-            axisLines.setCoordinateIndex(15, 7);
-            axisLines.setCoordinateIndex(16,10);
-            axisLines.setCoordinateIndex(17, 7);
-            axisLines.setCoordinateIndex(18,11);
-            axisLines.setCoordinateIndex(19, 7);
-            axisLines.setCoordinateIndex(20,12);
-            axisLines.setCoordinateIndex(21,13);
-            axisLines.setCoordinateIndex(22,14);
-            axisLines.setCoordinateIndex(23,13);
-            axisLines.setCoordinateIndex(24,15);
-            axisLines.setCoordinateIndex(25,13);
-            axisLines.setCoordinateIndex(26,16);
-            axisLines.setCoordinateIndex(27,13);
-            axisLines.setCoordinateIndex(28,17);
-            axisLines.setCoordinateIndex(29,13);
+		axisLines.setCoordinateIndex(0, 0);
+		axisLines.setCoordinateIndex(1, 1);
+		axisLines.setCoordinateIndex(2, 2);
+		axisLines.setCoordinateIndex(3, 1);
+		axisLines.setCoordinateIndex(4, 3);
+		axisLines.setCoordinateIndex(5, 1);
+		axisLines.setCoordinateIndex(6, 4);
+		axisLines.setCoordinateIndex(7, 1);
+		axisLines.setCoordinateIndex(8, 5);
+		axisLines.setCoordinateIndex(9, 1);
+		axisLines.setCoordinateIndex(10, 6);
+		axisLines.setCoordinateIndex(11, 7);
+		axisLines.setCoordinateIndex(12, 8);
+		axisLines.setCoordinateIndex(13, 7);
+		axisLines.setCoordinateIndex(14, 9);
+		axisLines.setCoordinateIndex(15, 7);
+		axisLines.setCoordinateIndex(16, 10);
+		axisLines.setCoordinateIndex(17, 7);
+		axisLines.setCoordinateIndex(18, 11);
+		axisLines.setCoordinateIndex(19, 7);
+		axisLines.setCoordinateIndex(20, 12);
+		axisLines.setCoordinateIndex(21, 13);
+		axisLines.setCoordinateIndex(22, 14);
+		axisLines.setCoordinateIndex(23, 13);
+		axisLines.setCoordinateIndex(24, 15);
+		axisLines.setCoordinateIndex(25, 13);
+		axisLines.setCoordinateIndex(26, 16);
+		axisLines.setCoordinateIndex(27, 13);
+		axisLines.setCoordinateIndex(28, 17);
+		axisLines.setCoordinateIndex(29, 13);
 
-            return axisLines;
+		axisLines.setColor(0, new Color4f(0.5f, 0.5f, 0.5f, 0.5f));
+		
+		return axisLines;
 
 	} // end of Axis createGeometry()
 
-
-    } // end of class Axis
+} // end of class Axis
