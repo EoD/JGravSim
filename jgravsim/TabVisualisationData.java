35edd5a7971d6927a6e41beaee11dc9264b802b7
EoD <EoD@xmw.de>
2011-08-31 18:45:42 +0200

    Fix: Use monospaced font in TabVisualisationData. Last fix for 'jumping around'.

--- jgravsim/TabVisualisationData.java
+++ jgravsim/TabVisualisationData.java
@@ -17,7 +17,7 @@ import javax.swing.border.BevelBorder;
 
 @SuppressWarnings("serial")
 public class TabVisualisationData extends JPanel {
-	public static final int revision = 1;
+	public static final int revision = 2;
 	
 	XMLParser myXMLParser;
 	
@@ -91,14 +91,14 @@ public class TabVisualisationData extends JPanel {
 				tf_Coordx.setHorizontalAlignment(JTextField.CENTER);
 				tf_Coordx.setEditable(false);
 				tf_Coordx.setPreferredSize(dSizeCoord);	
-				tf_Coordx.setFont(new Font("Sans Serif", Font.BOLD, 12));
+				tf_Coordx.setFont(new Font("Monospaced", Font.BOLD, 12));
 				tf_Coordx.setText("x in cm");
 				
 				tf_Coordy = new JTextField("0.0");
 				tf_Coordy.setHorizontalAlignment(JTextField.CENTER);
 				tf_Coordy.setEditable(false);
 				tf_Coordy.setPreferredSize(dSizeCoord);	
-				tf_Coordy.setFont(new Font("Sans Serif", Font.BOLD, 12));
+				tf_Coordy.setFont(new Font("Monospaced", Font.BOLD, 12));
 				tf_Coordy.setText("y in cm");
 				
 	
@@ -106,26 +106,25 @@ public class TabVisualisationData extends JPanel {
 				tf_Coordz.setHorizontalAlignment(JTextField.CENTER);
 				tf_Coordz.setEditable(false);
 				tf_Coordz.setPreferredSize(dSizeCoord);	
-				tf_Coordz.setFont(new Font("Sans Serif", Font.BOLD, 12));
+				tf_Coordz.setFont(new Font("Monospaced", Font.BOLD, 12));
 				tf_Coordz.setText("z in cm");
 						
 	
 				//Speed Eingabefelder	
 				la_speed_exact = new JLabel(myXMLParser.getText(242)+":");
-				la_speed_exact.setFont(new Font("Sans Serif", Font.BOLD, 12));
 
 				tf_Speedx = new JTextField("0.0");
 				tf_Speedx.setHorizontalAlignment(JTextField.CENTER);
 				tf_Speedx.setEditable(false);
 				tf_Speedx.setPreferredSize(dSizeSpeed);
-				tf_Speedx.setFont(new Font("Sans Serif", Font.BOLD, 12));
+				tf_Speedx.setFont(new Font("Monospaced", Font.BOLD, 12));
 				tf_Speedx.setText("x in m/s");
 				
 				tf_Speedy = new JTextField("0.0");
 				tf_Speedy.setHorizontalAlignment(JTextField.CENTER);
 				tf_Speedy.setEditable(false);
 				tf_Speedy.setPreferredSize(dSizeSpeed);
-				tf_Speedy.setFont(new Font("Sans Serif", Font.BOLD, 12));
+				tf_Speedy.setFont(new Font("Monospaced", Font.BOLD, 12));
 				tf_Speedy.setText("y in m/s");
 			    
 				
@@ -133,42 +132,39 @@ public class TabVisualisationData extends JPanel {
 				tf_Speedz.setHorizontalAlignment(JTextField.CENTER);
 				tf_Speedz.setEditable(false);
 				tf_Speedz.setPreferredSize(dSizeSpeed);
-				tf_Speedz.setFont(new Font("Sans Serif", Font.BOLD, 12));
+				tf_Speedz.setFont(new Font("Monospaced", Font.BOLD, 12));
 				tf_Speedz.setText("z in m/s");
 	
 
 				//Speed Slider
 				JLabel la_speed_abs = new JLabel(myXMLParser.getText(243)+":");
-				la_speed_exact.setFont(new Font("Sans Serif", Font.BOLD, 12));
 	
 				tf_Speed_abs = new JTextField(myXMLParser.getText(243)+" in m/s");
 				tf_Speed_abs.setHorizontalAlignment(JTextField.CENTER);
 				tf_Speed_abs.setEditable(false);
 				tf_Speed_abs.setPreferredSize(dSizeSpeed);
-				tf_Speed_abs.setFont(new Font("Sans Serif", Font.BOLD, 12));
+				tf_Speed_abs.setFont(new Font("Monospaced", Font.BOLD, 12));
 				
 			    //Mass Eingabefeld  
 	
 				JLabel lMass = new JLabel(myXMLParser.getText(246)+":");
-				lMass.setFont(new Font("Sans Serif", Font.BOLD, 12));
 			
 				tf_Mass = new JTextField("0.0");
 				tf_Mass.setHorizontalAlignment(JTextField.CENTER);
 				tf_Mass.setEditable(false);
 				tf_Mass.setPreferredSize(dSizeMass);	
-				tf_Mass.setFont(new Font("Sans Serif", Font.BOLD, 12));
+				tf_Mass.setFont(new Font("Monospaced", Font.BOLD, 12));
 				tf_Mass.setText(myXMLParser.getText(246)+" in kg");
 	
 				
 			    //Radius Eingabefeld
 				JLabel lRadius = new JLabel(myXMLParser.getText(247)+":");
-				lRadius.setFont(new Font("Sans Serif", Font.BOLD, 12));
 
 				tf_Radius = new JTextField("0.0");
 				tf_Radius.setHorizontalAlignment(JTextField.CENTER);
 				tf_Radius.setEditable(false);
 				tf_Radius.setPreferredSize(dSizeRadius);	
-				tf_Radius.setFont(new Font("Sans Serif", Font.BOLD, 12));
+				tf_Radius.setFont(new Font("Monospaced", Font.BOLD, 12));
 				tf_Radius.setText(myXMLParser.getText(247)+" in km");
 
 				la_Schwarzschild = new JLabel("("+myXMLParser.getText(108)+")");
@@ -179,13 +175,12 @@ public class TabVisualisationData extends JPanel {
 				
 			    //Dichte Eingabefeld
 				JLabel lDense = new JLabel(myXMLParser.getText(245)+":");
-				lRadius.setFont(new Font("Sans Serif", Font.BOLD, 12));
 
 				tf_Dense = new JTextField("0.0");
 				tf_Dense.setHorizontalAlignment(JTextField.CENTER);
 				tf_Dense.setEditable(false);
 				tf_Dense.setPreferredSize(dSizeDense);	
-				tf_Dense.setFont(new Font("Sans Serif", Font.BOLD, 12));
+				tf_Dense.setFont(new Font("Monospaced", Font.BOLD, 12));
 				//Dense in kg/m³
 				tf_Dense.setText(String.format(myXMLParser.getText(231),myXMLParser.getText(245),myXMLParser.getText(460),myXMLParser.getText(456)));
 
